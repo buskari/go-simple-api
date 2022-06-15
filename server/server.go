b3d2a5b1a10c89190c8cafbf82a52c8cdc96c8fc
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-06-15 15:03:00 -0300

    feat: add client

--- server/server.go
+++ server/server.go
@@ -7,32 +7,41 @@ import (
 	"net/url"
 	"os"
 	"text/template"
+	"time"
 
+	"https://github.com/buskari/go-simple-api/client"
 	"github.com/joho/godotenv"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
+var newsapi *client.Client
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	u, err := url.Parse(r.URL.String())
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func searchHandler(newsapi *client.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	params := u.Query()
-	searchQuery := params.Get("q")
-	page := params.Get("page")
-	if page == "" {
-		page = "1"
-	}
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
 
-	fmt.Println("Search Query is:", searchQuery)
-	fmt.Println("Page is:", page)
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		fmt.Println("%+v", results)
+	}
 }
 
 func main() {
@@ -45,6 +54,14 @@ func main() {
 	if port == "" {
 		port = "3000"
 	}
+
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apiKey must be set")
+	}
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi = client.NewClient(myClient, apiKey, 20)
 
 	var fs = http.FileServer(http.Dir("assets"))
 	var mux = http.NewServeMux()
